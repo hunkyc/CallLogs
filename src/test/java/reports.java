b1d8144473688786c6263be431e41dd314816351
martin <martin.smith@iname.com>
2015-01-28 22:28:58 +1000

    Updated reports

--- src/test/java/reports.java
+++ src/test/java/reports.java
@@ -25,8 +25,10 @@ public class reports {
     public void setUp() throws Exception {
 //        driver = new FirefoxDriver();
 //        driver = new HtmlUnitDriver();
-
-        System.setProperty("webdriver.chrome.driver", "/Users/martin/Downloads/chromedriver");
+//      location of chrome driver at work
+//        System.setProperty("webdriver.chrome.driver", "/Users/martin/Downloads/chromedriver");
+//      location of chrome driver at home
+        System.setProperty("webdriver.chrome.driver", "/home/martin/Applications/ChromeDriver/chromedriver");
         driver = new ChromeDriver();
 
 //        driver.manage().deleteAllCookies();
@@ -34,36 +36,24 @@ public class reports {
 //        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-//    @Test
-//    public void shouldDisplayReportsPage() throws InterruptedException {
-//        successfulLogin();
-//        driver.findElement(By.id(reportsButton)).click();
-//        assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
-//        Thread.sleep(3000);
-//    }
+    @Test
+    public void shouldDisplayReportsPage() throws InterruptedException {
+        successfulLogin();
+        driver.findElement(By.id(reportsButton)).click();
+        assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
+    }
 
-//    @Test
-//    public void shouldDisplayDailyReportsWhenDailyReportsSelected() throws InterruptedException {
-//        successfulLogin();
-//        driver.findElement(By.id(reportsButton)).click();
-//        assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
+    @Test
+    public void shouldDisplayDailyReportsWhenDailyReportsSelected() throws InterruptedException {
+        successfulLogin();
+        driver.findElement(By.id(reportsButton)).click();
+        assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
 //        driver.findElement(By.id("reportsSidePDFSelect")).click();
-//        driver.findElement(By.cssSelector("body > div.body > div.one_third > form > input")).click();
-//        Thread.sleep(3000);
-//    }
+        driver.findElement(By.cssSelector("body > div.body > div.one_third > form > input")).click();
+    }
 
     @Test
     public void shouldDisplayWeeklyReportsWhenWeeklyReportsSelected() throws InterruptedException {
-//        System.setProperty("webdriver.chrome.driver", "/Users/martin/Downloads/chromedriver");
-//        WebDriver driver = new ChromeDriver();
-
-//        driver.get("https://dashboard.nascomms.com/Login/Login?ReturnUrl=%2f");
-//        driver.findElement(By.id("UserName")).clear();
-//        driver.findElement(By.id("UserName")).sendKeys("Martin.Smith");
-//        driver.findElement(By.id("Password")).clear();
-//        driver.findElement(By.id("Password")).sendKeys("BongTow");
-//        driver.findElement(By.cssSelector("input.coloured_button")).click();
-
         successfulLogin();
         driver.findElement(By.id(reportsButton)).click();
         assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
@@ -71,6 +61,48 @@ public class reports {
         Thread.sleep(1000);
         driver.findElement(By.cssSelector("#reportsSidePDFSelect_listbox > li:nth-child(2)")).click();
         driver.findElement(By.cssSelector("body > div.body > div.one_third > form > input")).click();
+    }
+
+    @Test
+    public void shouldDisplayMonthlyReportsWhenMonthlyReportsSelected() throws InterruptedException {
+        successfulLogin();
+        driver.findElement(By.id(reportsButton)).click();
+        assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
+        driver.findElement(By.className("k-input")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#reportsSidePDFSelect_listbox > li:nth-child(3)")).click();
+        driver.findElement(By.cssSelector("body > div.body > div.one_third > form > input")).click();
+    }
+
+    @Test
+    public void shouldDisplayMonthlyFilteredReportsWhenMonthlyFilteredReportsSelected() throws InterruptedException {
+        successfulLogin();
+        driver.findElement(By.id(reportsButton)).click();
+        assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
+        driver.findElement(By.className("k-input")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#reportsSidePDFSelect_listbox > li:nth-child(4)")).click();
+        driver.findElement(By.cssSelector("body > div.body > div.one_third > form > input")).click();
+    }
+
+    @Test
+    public void shouldDisplayYearlyReportsWhenYearlyReportsSelected() throws InterruptedException {
+        successfulLogin();
+        driver.findElement(By.id(reportsButton)).click();
+        assertEquals("Reports - Nascomms Dashboard", driver.getTitle());
+        driver.findElement(By.className("k-input")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#reportsSidePDFSelect_listbox > li:nth-child(5)")).click();
+        driver.findElement(By.cssSelector("body > div.body > div.one_third > form > input")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+//        String verificationErrorString = verificationErrors.toString();
+//        if (!"".equals(verificationErrorString)) {
+//            fail(verificationErrorString);
+//        }
     }
 
     private void successfulLogin() {
